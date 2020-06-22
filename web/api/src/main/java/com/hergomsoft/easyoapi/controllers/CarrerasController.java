6ffaaa0eda2975daa5999575190678dfceb63f20
Zarkrosh <abelherrero98@gmail.com>
2020-06-22 18:11:11 +0200

    [API] CRUD Carreras
    Eliminación de servicio intermedio

--- web/api/src/main/java/com/hergomsoft/easyoapi/controllers/CarrerasController.java
+++ web/api/src/main/java/com/hergomsoft/easyoapi/controllers/CarrerasController.java
@@ -2,40 +2,59 @@ package com.hergomsoft.easyoapi.controllers;
 
 
 import com.hergomsoft.easyoapi.models.Carrera;
-import com.hergomsoft.easyoapi.services.ICarreraService;
+import com.hergomsoft.easyoapi.repository.CarreraRepository;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/carreras")
 public class CarrerasController {
     
     @Autowired
-    private ICarreraService carrerasService;
+    private CarreraRepository carrerasRepository;
     
     @GetMapping("")
     public List<Carrera> getCarreras() {
-        return carrerasService.findAll();
+        return carrerasRepository.findAll();
     }
 
     @PostMapping("")
-    public String nuevaCarrera() {
-        return "TODO Nueva carrera";
+    public ResponseEntity<Object> nuevaCarrera(@RequestBody Carrera carrera) {
+        // TODO Comprobar datos válidos
+        Carrera guardada = carrerasRepository.save(carrera);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(guardada.getId()).toUri();
+        
+        return ResponseEntity.created(location).build();
     }
     
-    @PutMapping("")
-    public String editarCarrera() {
-        return "TODO Editar carrera";
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> editarCarrera(@RequestBody Carrera carrera, @PathVariable long id) {
+        Optional<Carrera> opCarrera = carrerasRepository.findById(id);
+        if(opCarrera.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // TODO Comprobar datos válidos
+        carrera.setId(id);
+        carrerasRepository.save(carrera);
+        return ResponseEntity.noContent().build();
     }
     
-    @DeleteMapping("")
-    public String borrarCarrera() {
-        return "TODO Borrar carrera";
+    @DeleteMapping("/{id}")
+    public void borrarCarrera(@PathVariable long id) {
+        carrerasRepository.deleteById(id);
     }
 }
